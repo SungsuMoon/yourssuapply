d489a7f63e5fa6abfc27d410d606c791d1adb3ee
SungsuMoon <su931010@gmail.com>
2016-09-06 19:40:07 +0900

    First commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+function showhiddentext(x) {
+    $(x).css('opacity', '0.3');
+    $(x).next().css('display', 'block');
+}
+
+function normalImg(x) {
+    $(x).css('opacity', '1');
+    $(x).next().css('display', 'none');
+}
+
+function applywindow() {
+	location.href = "https://goo.gl/forms/DOeDi01uGW4oK76C3";
+}
